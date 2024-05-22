eb282ae271db0f62f0068026c4750498a1f14a61
mastanvali1 <mastanshaik1331@gmail.com>
2024-05-23 02:58:12 +0530

    updated the nav-bars

--- app.py
+++ app.py
@@ -72,6 +72,14 @@ def about():
     else:
         return redirect(url_for('login'))
 
+@app.route("/how")
+def htu():
+    if 'username' in session:
+        return render_template("howtouse.html")
+    else:
+        return redirect(url_for('login'))
+
+
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
     if 'username' in session:
